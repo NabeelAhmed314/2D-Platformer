905832033d51f93928613529a6ec3fc43a0f7bfe
Nabeel Ahmed <nabeelrajpoot314@gmail.com>
2020-09-07 10:53:31 +0500

    adding it first time

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public AudioMixer audiomixer;
+
+    public void OnPlayButtonClicked()
+    {
+        SceneManager.LoadScene("Main");
+    }
+    public void OnQuitButtonClicked()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+
+    public void OnMainMenuButtonClicked()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void SetVolume(float volume)
+    {
+        audiomixer.SetFloat("volume",volume);
+    }
+}
